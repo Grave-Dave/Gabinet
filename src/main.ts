a53a5ead43ea55a83a85706b006c107be3df815f
Dawid <dawgrab1@gmail.com>
2023-03-13 22:10:28 +0100

    header improved

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,70 @@
-import './scss/style.scss'
+import './scss/style.scss';
+import quotes from './quotes';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-   
-  </div>
-`
+// Header--------------------------------------
 
+let quote = 0;
+
+const getQuotes = () => {
+	document.querySelector<HTMLDivElement>('.quote')?.classList.add('display-down');
+
+	setTimeout(() => {
+		document.querySelector<HTMLDivElement>('.quote')?.classList.remove('display-down');
+		document.querySelector<HTMLDivElement>('.quote')?.classList.add('display-up');
+	}, 3000);
+
+	setTimeout(() => {
+		document.querySelector<HTMLDivElement>('.header_quotes')!.innerHTML = `
+    <p class="quote">
+    ${quotes[quote].quote}
+    </p>`;
+
+		quote++;
+		quote > 5 ? (quote = 0) : quote;
+		getQuotes();
+	}, 7000);
+};
+
+const handleBackground = () => {
+	document.querySelector('.header')?.classList.toggle('lighten');
+};
+
+document.querySelector('.header_btn')?.addEventListener('mouseenter', handleBackground);
+document.querySelector('.header_btn')?.addEventListener('mouseleave', handleBackground);
+
+// Nav--------------------------------------
+
+const handleNav = () => {
+  const goUpArrow = document.querySelector('.go-up-arrow');
+	const nav = document.querySelector('nav');
+
+	if (scrollY > 300) {
+		nav?.classList.add('nav-fixed');
+	} else {
+		nav?.classList.remove('nav-fixed');
+	}
+	if (scrollY > 1000) {
+		goUpArrow?.classList.add('visible');
+	} else goUpArrow?.classList.remove('visible');
+};
+
+
+const burgerBtn = document.querySelector('.hamburger')
+
+const handleNavMobile = () => {
+
+  const navItems = document.querySelector('.nav_items_mobile')
+
+  burgerBtn?.classList.toggle('is-active')
+  navItems?.classList.toggle('show')
+  
+}
+
+burgerBtn?.addEventListener('click', handleNavMobile)
+
+  
+
+
+window.addEventListener('scroll', handleNav);
+
+getQuotes();
---
+++ src/quotes.ts
@@ -0,0 +1,23 @@
+const quotes = [
+	{
+		quote: 'Doświadczasz nadmiernego smutku, lęku, poczucia winy, samotności.',
+	},
+	{
+		quote: 'Mierzysz się z depresją, zaburzeniami lękowymi, atakami paniki, zaburzeniem osobowości.',
+	},
+	{
+		quote: 'Czujesz się nieustannie zestresowany/na, wydarzyło się coś, z czym nie jesteś w stanie sobie poradzić.',
+	},
+	{
+		quote: 'Przestałeś/aś odczuwać satysfakcję ze swojego życia, tego kim jesteś, zgubiłeś/aś sens swojego życia.',
+	},
+	{
+		quote: 'Doświadczasz trudności w budowaniu i utrzymywaniu trwałych relacji bliskich i intymnych.',
+	},
+	{
+		quote:
+			'Chcesz lepiej zrozumieć siebie, poobserwować i zastanowić się z czego wynikają Twoje uczucia, myśli, zachowania, decyzje.',
+	},
+];
+
+export default quotes;
